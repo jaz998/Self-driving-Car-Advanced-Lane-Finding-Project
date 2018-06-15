14c668a74f55e746eb0360e6de100827bb0275b6
jaz998 <jaz998@users.noreply.github.com>
2018-06-16 00:03:52 +1000

    color gradient threshold
    compiled fine

--- advanced_lane_finding_project.py
+++ advanced_lane_finding_project.py
@@ -60,6 +60,31 @@ dist_pickle["mtx"] = mtx
 dist_pickle["dist"] = dist
 pickle.dump(dist_pickle, open('../camera_cal/disk_pickle2.p', 'wb'))
 
+################## Color/gradient threshold ###################
+def pipeline(img, s_thresh=(170,255), sx_thresh=(20,100)):
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # take the derivative in x
+    abs_sobelx = np.absolute(sobelx)
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel> s_thresh[0])&(scaled_sobel<s_thresh[1])]
+
+    # Threshold color channel
+    s_binnary = np.zeros_like(s_channel)
+    s_binnary[(s_channel>= s_thresh[0])& (s_channel<s_thresh[1])] = 1
+    # stack each channel
+    color_binary = np.dstak((np.zeros_like(sxbinary), sxbinary, sxbinary))*255
+    return color_binary
+
+
+
 
 
 
