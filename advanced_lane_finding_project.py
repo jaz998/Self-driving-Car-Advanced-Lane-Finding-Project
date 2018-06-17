bd2c8bb96881e70eb1a522c35ce0aa51201e6775
Jason Zhang <jaz998@users.noreply.github.com>
2018-06-17 12:23:48 +1000

    algorithmic perspective transform

--- advanced_lane_finding_project.py
+++ advanced_lane_finding_project.py
@@ -95,18 +95,101 @@ print ("Image saved")
 
 
 ############ Perspective Transform ######################
+
+
+# The below order_points function and four_point_transform functions are inspired by Adrian Rosebrock article 4 Point OpenCV getPerspective Transform Example (link: https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/)
+
+def order_points(pts):
+	# initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
+
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+
+	# return the ordered coordinates
+	return rect
+
+def four_point_transform(image, src):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = src
+	print("Order points are ", rect)
+	(tl, tr, br, bl) = rect
+
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	print("MaxWeidth:", maxWidth, " MaxHeight:", maxHeight)
+
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+	# return the warped image
+	return warped
+
+
 # Manually find four points representing a trapezoid in color_binary while they should be two straight lines
 # 255,686; 1044,686; 831,544; 463,544;
+# 255,686; 1044,686; 682,448; 599,448;
 
 
-src = np.float32([(255,686), (1044,686), (831, 544), (463,544)])
-dst = np.float32([(255,686), (1044,686), (1044,544), (255,268)])
+#src = np.float32([(255,686), (1044,686), (831, 544), (463,544)])
+#dst = np.float32([(255,686), (1044,686), (1044,544), (255,544)])
 
-M = cv2.getPerspectiveTransform(src, dst)
-warped = cv2.warpPerspective(road_image, M, test_image_size)
+#src = np.float32([(255,686), (1044,686), (682, 448), (599,448)])
+#dst = np.float32([(255,686), (1044,686), (1044,448), (255,448)])
+
+# src points in the order of top left, top right, bottom right, bottom left
+src = np.float32([(599,448), (682, 448), (1044,686), (255,686)])
+
+# M = cv2.getPerspectiveTransform(src, dst)
+# warped = cv2.warpPerspective(color_binary, M, (color_binary.shape[1], color_binary.shape[0]))
+warped = four_point_transform(road_image	, src)
+cv2.imwrite('../test_images/warped.png', warped)
 cv2.imshow('warped', warped)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+
+
+
+################# Detect Lane Line #############################
+
 
 
 
