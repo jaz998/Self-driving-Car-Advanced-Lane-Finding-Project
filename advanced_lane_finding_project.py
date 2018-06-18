7c1b1778d09f62b5d0d43aabe48074f4e48006cd
Jason Zhang <jaz998@users.noreply.github.com>
2018-06-18 18:34:46 +1000

    Visualizing sliding windows and finding lines done

--- advanced_lane_finding_project.py
+++ advanced_lane_finding_project.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
 import pickle
+from moviepy.editor import VideoFileClip
 
 # prepare object points
 nx = 9 #number of inside corners in x
@@ -247,6 +248,7 @@ def find_lines(binary_warped):
 	plt.plot(right_fitx, ploty, color='yellow')
 	plt.xlim(0, 1280)
 	plt.ylim(720, 0)
+	plt.show()
 
 	# Measuring Curvature
 	y_eval = np.max(ploty)
@@ -256,12 +258,90 @@ def find_lines(binary_warped):
 
 
 
+	return out_img, left_fit, right_fit
+
+
+results = find_lines(binary_warped)
+output_image = results[0]
+left_fit = results[1]
+right_fit = results[2]
+
+cv2.imshow('result', output_image)
+cv2.waitKey()
+
+
+def nextFrame (binary_warped, left_fit, right_fit):
+	# from the next frame of video (also called "binary_warped")
+	# It's now much easier to find line pixels!
+	nonzero = binary_warped.nonzero()
+	nonzeroy = np.array(nonzero[0])
+	nonzerox = np.array(nonzero[1])
+	margin = 100
+	left_lane_inds = ((nonzerox > (left_fit[0] * (nonzeroy ** 2) + left_fit[1] * nonzeroy +
+								   left_fit[2] - margin)) & (nonzerox < (left_fit[0] * (nonzeroy ** 2) +
+																		 left_fit[1] * nonzeroy + left_fit[
+																			 2] + margin)))
+
+	right_lane_inds = ((nonzerox > (right_fit[0] * (nonzeroy ** 2) + right_fit[1] * nonzeroy +
+									right_fit[2] - margin)) & (nonzerox < (right_fit[0] * (nonzeroy ** 2) +
+																		   right_fit[1] * nonzeroy + right_fit[
+																			   2] + margin)))
+
+	# Again, extract left and right line pixel positions
+	leftx = nonzerox[left_lane_inds]
+	lefty = nonzeroy[left_lane_inds]
+	rightx = nonzerox[right_lane_inds]
+	righty = nonzeroy[right_lane_inds]
+	# Fit a second order polynomial to each
+	left_fit = np.polyfit(lefty, leftx, 2)
+	right_fit = np.polyfit(righty, rightx, 2)
+	# Generate x and y values for plotting
+	ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
+	left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+	right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+	# Create an image to draw on and an image to show the selection window
+	out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+	window_img = np.zeros_like(out_img)
+	# Color in left and right line pixels
+	out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+	out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+	# Generate a polygon to illustrate the search window area
+	# And recast the x and y points into usable format for cv2.fillPoly()
+	left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
+	left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin,
+																	ploty])))])
+	left_line_pts = np.hstack((left_line_window1, left_line_window2))
+	right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
+	right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin,
+																	 ploty])))])
+	right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+	# Draw the lane onto the warped blank image
+	cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+	cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+	result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+	plt.imshow(result)
+	plt.plot(left_fitx, ploty, color='yellow')
+	plt.plot(right_fitx, ploty, color='yellow')
+	plt.xlim(0, 1280)
+	plt.ylim(720, 0)
+	plt.show()
+
 	return out_img
 
-
-result = find_lines(binary_warped)
-cv2.imshow('result', result)
+nextFrame_image = nextFrame(binary_warped, left_fit, right_fit)
+cv2.imshow("Next frame", nextFrame_image)
 cv2.waitKey()
+
+############# Process the video #####################################
+
+
+
+
+
+
 
 
 
